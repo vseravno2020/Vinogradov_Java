64c8187292f818c21a766756c7298d264fd4d104
vinogradov a <vseravno2019@gmail.com>
2021-10-10 22:00:24 +0700

    lab5

---
+++ src/lab_5/Zadanie_1.java
@@ -0,0 +1,43 @@
+package lab_5;
+
+import java.util.Scanner;
+
+public class Zadanie_1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Write number: ");
+        int number = in.nextInt();
+        switch(number){
+            case 0:
+                System.out.println("Ноль");
+                break;
+            case 1:
+                System.out.println("Один");
+                break;
+            case 2:
+                System.out.println("Два");
+                break;
+            case 3:
+                System.out.println("Три");
+                break;
+            case 4:
+                System.out.println("Четыре");
+                break;
+            case 5:
+                System.out.println("Пять");
+                break;
+            case 6:
+                System.out.println("Шесть");
+                break;
+            case 7:
+                System.out.println("Семь");
+                break;
+            case 8:
+                System.out.println("Восемь");
+                break;
+            case 9:
+                System.out.println("Девять");
+                break;
+        }
+    }
+}
